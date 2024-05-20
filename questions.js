b5cd48ddeae8886f0a834d9c19adfee573ad7917
Landim9 <landimr9des@gmail.com>
2024-05-20 08:32:16 -0300

    QUIZAPP

---
+++ questions.js
@@ -0,0 +1,64 @@
+export default [
+    {
+        question: "Quem canta a música Baby?",
+        answers: [
+            {option: "Justin Biber", correct:true},
+            {option: "Selena Gomez", correct:false},
+            {option: "The Weekend", correct:false},
+        ],
+    },
+
+    {
+        question: "Quantos Grammy's a Billie Eilsh tem?",
+        answers: [
+            {option: "3", correct:false},
+            {option: "9", correct:true},
+            {option: "8", correct:false},
+        ],
+    },
+    
+    {
+        question: "Em que ano foi lançado o ultimo album de Rihanna",
+        answers: [
+            {option: "2009", correct:false},
+            {option: "2014", correct:false},
+            {option: "2016", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Que tipo de música o Travis Scott canta?",
+        answers: [
+            {option: "Rap", correct:true},
+            {option: "Trap", correct:false},
+            {option: "Pop music", correct:false},
+        ],
+        
+    },
+    {
+        question: "Quantos integrantes tem na banda Queen?",
+        answers: [
+            {option: "4", correct:true},
+            {option: "6", correct:false},
+            {option: "5", correct:false},
+        ],
+    },
+    {
+        question: "Idade da Demi Lovato",
+        answers: [
+            {option: "31", correct:true},
+            {option: "32", correct:false},
+            {option: "28", correct:false},
+        ],
+    },
+
+    {
+        question: "Quando o Michel Jackson morreu?",
+        answers: [
+            {option: "2010", correct:false},
+            {option: "2008", correct:false},
+            {option: "2009", correct:true},
+        ],
+    }
+];
